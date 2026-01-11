0f922bdfccc9bdc1d0b15a2cb78f1e9798743ff7
Rohan-R07 <rveda013@gmail.com>
2026-01-12 00:40:18 +0530

    Initialize dsa_study repo and add Java DSA Notes

---
+++ JumpStatements.java
@@ -0,0 +1,7 @@
+public class JumpStatements {
+    public static void main(String[] args){
+        //break statement - exits loop based on condition
+        // continue statement - skips an iteration for a condition
+        //return - transfer control from one method to another method 
+    }
+}
